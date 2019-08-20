d64c4dcea43f6c37284bf964573bb2efd02a2f27
Louis Lammerting <louis.lammerting@gmail.com>
2019-08-20 12:50:23 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,7 +8,11 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
+  puts "Cards add up to #{card_total}"
+end
+
+def display_dealer_card_total(card_total)
+  puts "The dealer's cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -17,6 +21,7 @@ end
 
 def get_user_input
   input = gets.chomp
+  input
 end
 
 def end_game(card_total)
@@ -24,26 +29,75 @@ def end_game(card_total)
 end
 
 def initial_round
- card_total = 0
- card_total += deal_card
- card_total += deal_card
- display_card_total(card_total)
-end
-end
-
-def hit?
-  # code hit? here
+card_total = 0
+card_total += deal_card
+card_total += deal_card
+display_card_total(card_total)
+card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "That is an invalid comment. You have only the following options:"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def win
+  puts "Congratulations. You have won the game."
+end
+
+def loss
+  puts "You lost the Game"
+end
+
+def hit_order
+  points = initial_round
+    until points > 21
+    prompt_user
+    response = get_user_input
+      if response == "h"
+      points += deal_card
+          if points <= 21
+            display_card_total(points)
+            points
+          else
+          end_game(points)
+          break
+          end
+      elsif response == "s"
+        puts "Your final hand adds up to #{points}."
+        break
+      else
+      invalid_command
+      end
+    end
+   points 
+end
+
+def dealer
+  puts "Now it's the dealer's turn"
+  points = initial_round
+  while points < 17
+    points += deal_card
+  end
+  display_dealer_card_total(points)
+  points
+end  
 
 def runner
-  # code runner here
+  player_points = hit_order.to_i
+  dealer_points = dealer.to_i
+  if player_points > 21
+    loss
+  elsif player_points < 21 && dealer_points > 21
+    win
+  elsif player_points == dealer_points
+    puts "It's a draw."
+  else
+    if player_points > dealer_points
+      win
+    else
+      loss
+    end
+  end
 end
-    
+
+runner
